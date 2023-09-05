a19e73133633a7e95924b141f26d283666210b0f
Guo Yunhe <i@guoyunhe.me>
2023-09-05 22:05:27 +0800

    prettier config

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:jest/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    createDefaultProgram: true,
+  },
+  plugins: ["@typescript-eslint", "react", "prettier"],
+  ignorePatterns: ["build", "coverage", "dist", "lib", "node_modules"],
+};
